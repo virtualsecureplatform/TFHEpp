a17a80f6d8d05fa47dc3c4e5bcdf20dfa7ce824a
nindanaoto <dirty.knife603@gmail.com>
2020-09-26 20:47:42 +0900

    Added serialize to keys

---
+++ test/cereal.cpp
@@ -0,0 +1,25 @@
+#include <tfhe++.hpp>
+#include <fstream>
+#include <cassert>
+
+using namespace TFHEpp;
+
+int main()
+{
+
+    SecretKey sk;
+    {
+        std::ofstream ofs{"./secret.key", std::ios::binary};
+        cereal::PortableBinaryOutputArchive ar(ofs);
+        sk.serialize(ar);
+    }
+    SecretKey ski;
+    {
+        std::ifstream ifs{"./secret.key", std::ios::binary};
+        cereal::PortableBinaryInputArchive ar(ifs);
+        ski.serialize(ar);
+    }
+    for(int i = 0;i<DEF_n;i++)assert(sk.key.lvl0[i]==ski.key.lvl0[i]);
+    for(int i = 0;i<DEF_N;i++)assert(sk.key.lvl1[i]==ski.key.lvl1[i]);
+    for(int i = 0;i<DEF_nbar;i++)assert(sk.key.lvl2[i]==ski.key.lvl2[i]);
+}
