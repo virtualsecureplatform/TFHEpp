0e89101d8c1209ef3828cdc0a7984c14e0faf393
nindanaoto <dirty.knife603@gmail.com>
2020-11-21 16:05:54 +0900

    Use std::fma

--- include/utils.hpp
+++ include/utils.hpp
@@ -9,7 +9,6 @@
 #include <random>
 
 namespace TFHEpp {
-using namespace std;
 static randen::Randen<uint64_t> generator;
 
 // Double to Torus(32bit fixed-point number)
@@ -21,7 +20,7 @@ inline uint32_t dtot32(double d)
 // Modular Gaussian Distribution over Torus(32bit fixed-point number)
 inline uint32_t gaussian32(uint32_t message, double α)
 {
-    normal_distribution<double> distribution(
+    std::normal_distribution<double> distribution(
         0., α);  // TODO: can we create a global distrib of param 1 and multiply
                  // by sigma?
     double err = distribution(generator);
@@ -32,7 +31,7 @@ inline uint32_t gaussian32(uint32_t message, double α)
 inline uint64_t gaussian64(uint64_t center, double stdev)
 {
     static const double _2p64 = pow(2., 64);
-    normal_distribution<double> distribution(0., 1.0);
+    std::normal_distribution<double> distribution(0., 1.0);
     const double val = stdev * distribution(generator) * _2p64;
     const uint64_t ival = static_cast<uint64_t>(val);
     return ival + center;
@@ -63,8 +62,8 @@ inline void MulInFD(array<double, N> &res, const array<double, N> &a,
     for (int i = 0; i < N / 2; i++) {
         double aimbim = a[i + N / 2] * b[i + N / 2];
         double arebim = a[i] * b[i + N / 2];
-        res[i] = a[i] * b[i] - aimbim;
-        res[i + N / 2] = a[i + N / 2] * b[i] + arebim;
+        res[i] = std::fma(a[i], b[i], - aimbim);
+        res[i + N / 2] = std::fma(a[i + N / 2], b[i], arebim);
     }
 }
 
@@ -73,10 +72,10 @@ inline void FMAInFD(array<double, N> &res, const array<double, N> &a,
                     const array<double, N> &b)
 {
     for (int i = 0; i < N / 2; i++) {
-        res[i] = a[i + N / 2] * b[i + N / 2] - res[i];
-        res[i] = a[i] * b[i] - res[i];
-        res[i + N / 2] += a[i] * b[i + N / 2];
-        res[i + N / 2] += a[i + N / 2] * b[i];
+        res[i] = std::fma(a[i + N / 2], b[i + N / 2], - res[i]);
+        res[i] = std::fma(a[i], b[i], - res[i]);
+        res[i + N / 2] = std::fma(a[i], b[i + N / 2], res[i + N / 2]);
+        res[i + N / 2] = std::fma(a[i + N / 2], b[i], res[i + N / 2]);
     }
 }
 }  // namespace TFHEpp
