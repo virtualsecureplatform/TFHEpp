bed7703e3cda6f8b725bb92f69f88dcb81214502
nindanaoto <dirty.knife603@gmail.com>
2021-06-26 23:39:35 +0900

    Added io-packet

---
+++ include/io-packet.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "params.hpp"
+#include <map>
+#include <string>
+#include <vector>
+
+namespace THFEpp {
+	struct IOpacket{
+		std::map<std::string,std::vector<TFHEpp::TLWE<TFHEpp::lvl0param>>> tlwelvl0ports;
+		std::map<std::string,std::vector<TFHEpp::TRLWE<TFHEpp::lvl2param>>> trlwelvl2ports;
+		std::map<std::string,std::vector<TFHEpp::TRGSW<TFHEpp::lvl2param>>> trgswlvl2ports;
+		
+		template <class Archive>
+		void serialize(Archive &archive)
+		{
+			archive(tlwelvl0ports, trlwelvl2ports,trgswlvl2ports);
+		}
+	};
+}
