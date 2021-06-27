d21627dd6bf0537eb68ea6e56c38de43b9328fde
nindanaoto <dirty.knife603@gmail.com>
2021-06-27 16:23:43 +0900

    fix iopacket.hpp

--- include/io-packet.hpp
+++ include/io-packet.hpp
@@ -1,20 +1,29 @@
 #pragma once
 
+#include "../thirdparties/cereal/include/cereal/archives/portable_binary.hpp"
+#include "../thirdparties/cereal/include/cereal/cereal.hpp"
+#include "../thirdparties/cereal/include/cereal/types/unordered_map.hpp"
+#include "../thirdparties/cereal/include/cereal/types/string.hpp"
+#include "../thirdparties/cereal/include/cereal/types/vector.hpp"
+#include "../thirdparties/cereal/include/cereal/types/array.hpp"
+#include "../thirdparties/cereal/include/cereal/types/optional.hpp"
+
 #include "params.hpp"
-#include <map>
+#include <unordered_map>
 #include <string>
 #include <vector>
 
 namespace TFHEpp {
+
 	struct IOpacket{
-		std::map<std::string,std::vector<TFHEpp::TLWE<TFHEpp::lvl0param>>> tlwelvl0ports;
-		std::map<std::string,std::vector<TFHEpp::TRLWE<TFHEpp::lvl2param>>> trlwelvl2ports;
-		std::map<std::string,std::vector<TFHEpp::TRGSW<TFHEpp::lvl2param>>> trgswlvl2ports;
+		std::unordered_map<std::string,std::vector<TFHEpp::TLWE<TFHEpp::lvl0param>>> tlwelvl0ports;
+		std::unordered_map<std::string,std::vector<TFHEpp::TRLWE<TFHEpp::lvl2param>>> trlwelvl2ports;
+		std::unordered_map<std::string,std::vector<TFHEpp::TRGSW<TFHEpp::lvl2param>>> trgswlvl2ports;
 		
 		template <class Archive>
 		void serialize(Archive &archive)
 		{
-			archive(tlwelvl0ports, trlwelvl2ports,trgswlvl2ports);
+			archive(tlwelvl0ports,trlwelvl2ports,trgswlvl2ports);
 		}
 	};
 }
--- test/cereal.cpp
+++ test/cereal.cpp
@@ -3,29 +3,41 @@
 #include <iostream>
 #include <tfhe++.hpp>
 
-using namespace TFHEpp;
 
 int main()
 {
-    SecretKey sk;
+    TFHEpp::SecretKey sk;
     {
-        std::ofstream ofs{"./secret.key", std::ios::binary};
-        cereal::PortableBinaryOutputArchive ar(ofs);
-        sk.serialize(ar);
+        {
+            std::ofstream ofs{"./secret.key", std::ios::binary};
+            cereal::PortableBinaryOutputArchive ar(ofs);
+            sk.serialize(ar);
+        }
+        TFHEpp::SecretKey ski;
+        {
+            std::ifstream ifs{"./secret.key", std::ios::binary};
+            cereal::PortableBinaryInputArchive ar(ifs);
+            ski.serialize(ar);
+        }
+        for (int i = 0; i < TFHEpp::lvl0param::n; i++)
+            assert(sk.key.lvl0[i] == ski.key.lvl0[i]);
+        for (int i = 0; i < TFHEpp::lvl1param::n; i++)
+            assert(sk.key.lvl1[i] == ski.key.lvl1[i]);
+        for (int i = 0; i < TFHEpp::lvl2param::n; i++)
+            assert(sk.key.lvl2[i] == ski.key.lvl2[i]);
+        std::cout << "n"
+                << ":" << ski.params.lvl0.n << std::endl;
+        assert(sk.params == ski.params);
     }
-    SecretKey ski;
+
     {
-        std::ifstream ifs{"./secret.key", std::ios::binary};
-        cereal::PortableBinaryInputArchive ar(ifs);
-        ski.serialize(ar);
+        TFHEpp::IOpacket iopacket;
+        iopacket.tlwelvl0ports["test"].resize(1);
+        iopacket.tlwelvl0ports["test"][0] = TFHEpp::tlweSymEncrypt<TFHEpp::lvl0param>(TFHEpp::lvl0param::μ,TFHEpp::lvl0param::α,sk.key.lvl0);
+        {
+            std::ofstream ofs{"./iopacket.data", std::ios::binary};
+            cereal::PortableBinaryOutputArchive ar(ofs);
+            iopacket.serialize(ar);
+        }
     }
-    for (int i = 0; i < lvl0param::n; i++)
-        assert(sk.key.lvl0[i] == ski.key.lvl0[i]);
-    for (int i = 0; i < lvl1param::n; i++)
-        assert(sk.key.lvl1[i] == ski.key.lvl1[i]);
-    for (int i = 0; i < lvl2param::n; i++)
-        assert(sk.key.lvl2[i] == ski.key.lvl2[i]);
-    std::cout << "n"
-              << ":" << ski.params.lvl0.n << std::endl;
-    assert(sk.params == ski.params);
 }
