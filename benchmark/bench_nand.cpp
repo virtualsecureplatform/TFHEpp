4c3723571f15ec3a8d8ac9e35b721a8f0b5c335e
nindanaoto <dirty.knife603@gmail.com>
2021-07-09 14:41:09 +0900

    added mux to bench

--- benchmark/bench_nand.cpp
+++ benchmark/bench_nand.cpp
@@ -14,5 +14,19 @@ void BM_HomGate(benchmark::State& state){
     for (auto _ : state) TFHEpp::HomNAND(res, ca, cb, *gk);
 }
 
-BENCHMARK(BM_HomGate);
+void BM_HomMUX(benchmark::State& state){
+    std::random_device seed_gen;
+    std::default_random_engine engine(seed_gen());
+    std::uniform_int_distribution<uint32_t> binary(0, 1);
+    const TFHEpp::SecretKey* sk = new TFHEpp::SecretKey();
+    const TFHEpp::GateKey* gk = new TFHEpp::GateKey(*sk);
+    TFHEpp::TLWE<TFHEpp::lvl0param> ca = TFHEpp::tlweSymEncrypt<TFHEpp::lvl0param>(binary(engine),TFHEpp::lvl0param::α,sk->key.lvl0);
+    TFHEpp::TLWE<TFHEpp::lvl0param> cb = TFHEpp::tlweSymEncrypt<TFHEpp::lvl0param>(binary(engine),TFHEpp::lvl0param::α,sk->key.lvl0);
+    TFHEpp::TLWE<TFHEpp::lvl0param> cs = TFHEpp::tlweSymEncrypt<TFHEpp::lvl0param>(binary(engine),TFHEpp::lvl0param::α,sk->key.lvl0);
+    TFHEpp::TLWE<TFHEpp::lvl0param> res;
+    for (auto _ : state) TFHEpp::HomMUX(res, cs, ca, cb, *gk);
+}
+
+BENCHMARK(BM_HomGate)->Iterations(1)->Repetitions(10);
+BENCHMARK(BM_HomMUX)->Iterations(1)->Repetitions(10);
 BENCHMARK_MAIN();
