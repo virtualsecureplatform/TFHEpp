e75d209bae84bd15c8f67b360427b225c3d17673
nindanaoto <dirty.knife603@gmail.com>
2021-11-19 20:42:58 +0900

    remove gcc extention dependency

--- include/params/128bit.hpp
+++ include/params/128bit.hpp
@@ -42,8 +42,8 @@ struct lvl2param {
     static const inline double α = std::pow(2.0, -44);  // fresh noise
     using T = uint64_t;                                 // Torus representation
     static constexpr T μ = 1ULL << 61;
-    static constexpr uint32_t plain_modulus = 2;
-    static constexpr double Δ = std::pow(2., std::numeric_limits<T>::digits)/plain_modulus;
+    static constexpr uint32_t plain_modulus = 8;
+    static constexpr double Δ = μ;
 };
 
 // Key Switching parameters
