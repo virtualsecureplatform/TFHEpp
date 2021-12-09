795388c0a02c353310a967f30893d0e67993c794
Naoki Matsumoto <m.naoki9911@gmail.com>
2021-12-09 22:16:56 +0900

    Add spqliox_aarch64
    spqliox_aarch64 is spqlios written with xbyak_aarch64.
    To use, install `xbyak_aarch64` and add `-DUSE_SPQLIOX_AARCH64=on` to the compile option.

--- format.bash
+++ format.bash
@@ -3,6 +3,8 @@
 clang-format -style=file -i ./src/*.cpp
 clang-format -style=file -i ./thirdparties/fftw/*.h
 clang-format -style=file -i ./thirdparties/fftw/*.cpp
+clang-format -style=file -i ./thirdparties/spqliox_aarch64/*.h
+clang-format -style=file -i ./thirdparties/spqliox_aarch64/*.cpp
 clang-format -style=file -i ./include/*.hpp
 clang-format -style=file -i ./include/params/*.hpp
 clang-format -style=file -i ./test/*.cpp
@@ -14,3 +16,4 @@ cmake-format -i src/CMakeLists.txt
 cmake-format -i benchmark/CMakeLists.txt
 cmake-format -i thirdparties/spqlios/CMakeLists.txt
 cmake-format -i thirdparties/fftw/CMakeLists.txt
+cmake-format -i thirdparties/spqliox_aarch64/CMakeLists.txt
---
+++ thirdparties/spqliox_aarch64/fft_processor_spqliox_aarch64.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+
+#include "spqliox_aarch64_impl.h"
+
+struct alignas(32) FFT_PRECOMP {
+    uint64_t n;
+    double *trig_tables;
+};
+
+class FFT_Processor_Spqliox_AArch64 {
+public:
+    const int32_t _2N;
+    const int32_t N;
+    const int32_t Ns2;
+
+private:
+    double *real_inout;
+    FFT_PRECOMP tables_direct_, tables_reverse_, table_negation_forward_,
+        table_negation_reverse_;
+    unsigned long (*fft_)(double *, const double *, const double *,
+                          const double *, const FFT_PRECOMP *, double *,
+                          double *);
+    unsigned long (*ifft_)(double *, const double *, const double *,
+                           const FFT_PRECOMP *, double *, double *);
+    spqliox_aarch64::fft_code fft_code_;
+    spqliox_aarch64::ifft_code ifft_code_;
+
+public:
+    FFT_Processor_Spqliox_AArch64(const int32_t N);
+
+    void execute_reverse_int(double *res, const int32_t *a);
+
+    void execute_reverse_torus32(double *res, const uint32_t *a);
+
+    void execute_direct_torus32(uint32_t *res, const double *a);
+
+    void execute_direct_torus32_rescale(uint32_t *res, const double *a,
+                                        const double Δ);
+
+    void execute_reverse_torus64(double *res, const uint64_t *a);
+
+    void execute_direct_torus64(uint64_t *res, const double *a);
+
+    ~FFT_Processor_Spqliox_AArch64();
+};
+
+extern thread_local FFT_Processor_Spqliox_AArch64 fftplvl1;
+extern thread_local FFT_Processor_Spqliox_AArch64 fftplvl2;
