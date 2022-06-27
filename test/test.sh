ba00b5b4dc634f46555f3fb8f3a8ba2582cf1843
Naoki MATSUMOTO <naoki@pibvt.net>
2022-06-27 20:34:18 +0900

    Ignore some tests

--- test/test.sh
+++ test/test.sh
@@ -2,12 +2,30 @@
 
 set -eux
 
+# thanks to https://qiita.com/Hayao0819/items/0e04b39b0804a0d16020
+contains() {
+    local i
+    for i in $2{[@]}; do
+        if [[ ${i} = ${1} ]]; then
+            echo "TRUE"
+            return
+        fi
+    done
+    echo "FALSE"
+    return
+}
+
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR
 
 TEST_BINARIES=`find . -maxdepth 1 -perm -111 -type f`
+IGNORED_BINARIES=('./combinedmem' './tlwemult' './bitsequencerepresentation' '')
 for TEST_BINARY in $TEST_BINARIES
 do
+    RES=`contains $TEST_BINARY "${IGNORED_BINARIES[*]}"`
+    if [[ $RES == "TRUE" ]]; then
+        continue
+    fi
     echo "Testing $TEST_BINARY"
     ./$TEST_BINARY
 done
