d6dec8e511710daadca17cce4f3e759dad3c4ef1
nindanaoto <dirty.knife603@gmail.com>
2023-03-07 19:39:36 +0900

    Updated CONCRETE CB

--- include/params/concrete.hpp
+++ include/params/concrete.hpp
@@ -48,14 +48,14 @@ struct lvl1param {
 struct lvl2param {
     static constexpr int32_t key_value_max = 1;
     static constexpr int32_t key_value_min = 0;
-    static const std::uint32_t nbit = 10;  // dimension must be a power of 2 for
+    static const std::uint32_t nbit = 9;  // dimension must be a power of 2 for
                                            // ease of polynomial multiplication.
     static constexpr std::uint32_t n = 1 << nbit;  // dimension
-    static constexpr std::uint32_t k = 2;
-    static constexpr std::uint32_t l = 4;
+    static constexpr std::uint32_t k = 3;
+    static constexpr std::uint32_t l = 3;
     static constexpr std::uint32_t Bgbit = 9;
     static constexpr std::uint32_t Bg = 1 << Bgbit;
-    static const inline double α = std::pow(2.0, -44);  // fresh noise
+    static const inline double α = std::pow(2.0, -37);  // fresh noise
     using T = uint64_t;                                 // Torus representation
     static constexpr T μ = 1ULL << 61;
     static constexpr uint32_t plain_modulus = 8;
@@ -91,10 +91,10 @@ struct lvl20param {
 };
 
 struct lvl21param {
-    static constexpr std::uint32_t t = 24;  // number of addition in
+    static constexpr std::uint32_t t = 6;  // number of addition in
                                             // keyswitching
     static constexpr std::uint32_t basebit =
-        1;  // how many bit should be encrypted in keyswitching key
+        4;  // how many bit should be encrypted in keyswitching key
     static const inline double α = lvl1param::α;  // key noise
     using domainP = lvl2param;
     using targetP = lvl1param;
