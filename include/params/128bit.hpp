1b4e2b20b766e3a8c3be500e72a6551a4459e46d
nindanaoto <matsuoka.kotaro@gmail.com>
2023-04-02 09:24:42 +0000

    fix lvl2param

--- include/params/128bit.hpp
+++ include/params/128bit.hpp
@@ -41,7 +41,7 @@ struct lvl1param {
 
 struct lvl2param {
     static constexpr int32_t key_value_max = 1;
-    static constexpr int32_t key_value_min = 0;
+    static constexpr int32_t key_value_min = -1;
     static const std::uint32_t nbit = 11;  // dimension must be a power of 2 for
                                            // ease of polynomial multiplication.
     static constexpr std::uint32_t n = 1 << nbit;  // dimension
@@ -49,7 +49,7 @@ struct lvl2param {
     static constexpr std::uint32_t l = 4;
     static constexpr std::uint32_t Bgbit = 9;
     static constexpr std::uint32_t Bg = 1 << Bgbit;
-    static const inline double α = std::pow(2.0, -44);  // fresh noise
+    static const inline double α = std::pow(2.0, -47);  // fresh noise
     using T = uint64_t;                                 // Torus representation
     static constexpr T μ = 1ULL << 61;
     static constexpr uint32_t plain_modulus = 8;
