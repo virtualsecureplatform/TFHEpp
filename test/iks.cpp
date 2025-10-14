2ec1086b06f38b1045e494d3a8e0e3750f66e6e7
nindanaoto <matsuoka.kotaro@gmail.com>
2025-10-14 15:02:40 +0900

    Added iks measurement

--- test/iks.cpp
+++ test/iks.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <chrono>
 #include <iostream>
 #include <random>
 #include <tfhe++.hpp>
@@ -13,18 +14,30 @@ int main()
     TFHEpp::SecretKey sk;
     TFHEpp::EvalKey ek;
     ek.emplaceiksk<TFHEpp::lvl10param>(sk);
+    std::vector<TFHEpp::TLWE<TFHEpp::lvl1param>> tlwe(num_test);
+    std::vector<TFHEpp::TLWE<TFHEpp::lvl0param>> res(num_test);
+    std::array<bool, num_test> p;
+    for (int i = 0; i < num_test; i++) p[i] = binary(engine) > 0;
+    for (int i = 0; i < num_test; i++)
+        tlwe[i] = TFHEpp::tlweSymEncrypt<TFHEpp::lvl1param>(
+            p[i] ? TFHEpp::lvl1param::μ : -TFHEpp::lvl1param::μ,
+            sk.key.get<TFHEpp::lvl1param>());
+
+    std::chrono::system_clock::time_point start, end;
+    start = std::chrono::system_clock::now();
     for (int test = 0; test < num_test; test++) {
-        bool p = binary(engine) > 0;
-        TFHEpp::TLWE<TFHEpp::lvl1param> tlwe =
-            TFHEpp::tlweSymEncrypt<TFHEpp::lvl1param>(
-                p ? TFHEpp::lvl1param::μ : -TFHEpp::lvl1param::μ,
-                sk.key.get<TFHEpp::lvl1param>());
-        TFHEpp::TLWE<TFHEpp::lvl0param> res;
         TFHEpp::IdentityKeySwitch<TFHEpp::lvl10param>(
-            res, tlwe, ek.getiksk<TFHEpp::lvl10param>());
+            res[test], tlwe[test], ek.getiksk<TFHEpp::lvl10param>());
+    }
+    end = std::chrono::system_clock::now();
+    double elapsed =
+        std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+            .count();
+    std::cout << elapsed / num_test << "ms" << std::endl;
+    for (int i = 0; i < num_test; i++) {
         bool p2 = TFHEpp::tlweSymDecrypt<TFHEpp::lvl0param>(
-            res, sk.key.get<TFHEpp::lvl0param>());
-        assert(p == p2);
+            res[i], sk.key.get<TFHEpp::lvl0param>());
+        assert(p[i] == p2);
     }
     std::cout << "Passed" << std::endl;
 }
