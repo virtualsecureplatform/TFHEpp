eca8a67476e42cca5912551144f71c7ac0f9341e
nindanaoto <matsuoka.kotaro@gmail.com>
2026-06-28 09:09:47 +0000

    Set lvl0 dimension to 630

--- include/params/concrete.hpp
+++ include/params/concrete.hpp
@@ -12,7 +12,7 @@ struct lvl0param {
     static constexpr int32_t key_value_max = 1;
     static constexpr int32_t key_value_min = 0;
     static constexpr int32_t key_value_diff = key_value_max - key_value_min;
-    static constexpr std::uint32_t n = 636;  // dimension
+    static constexpr std::uint32_t n = 630;  // dimension
     static constexpr std::uint32_t k = 1;
     static constexpr ErrorDistribution errordist =
         ErrorDistribution::ModularGaussian;
